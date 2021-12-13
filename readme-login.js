d510cc1812c1cf0a61b4d08fea97be2f340ecd2c
Dmytro Dovbii <dmytro.d@quali.com>
2021-12-13 14:55:10 +0200

    add torque login

--- readme-login.js
+++ readme-login.js
@@ -4,30 +4,48 @@ var { URL } = require('url');
 // POST request to the login form
 // This should do all of the work to login and verify the user is valid
 module.exports = (req, res) => {
-  var { email, password } = req.body;
+  var { email, password, account } = req.body;
+  console.log('Use these credentials to log the user in somewhere:', { email, password, account });
 
-  console.log('Use these credentials to log the user in somewhere:', { email, password });
+  var url = `https://app.qtorque.io/api/accounts/${account}/login`;
 
-  // Validate email and password is a correct user, and get the user information
+  var XMLHttpRequest = require('xhr2');
+  var xhr = new XMLHttpRequest();
 
-  // User being logged into ReadMe
-  // At this point we're hardcoding in a user's name to mock sending in a valid user
-  var user = {
-    name: email,
-    email: email,
-    
-    // User's API Key (OR) { user, pass }
-    apiKey: { user: 'testUser', pass: '123pass' },
-    
-    version: 1, // Required, if omited things can break unexpectedly
+  xhr.responseType = 'json';
 
-    // You can pass in any information here and use it in your documentation!
-    // Full list of data that has special meaning in our API Explorer: https://docs.readme.com/docs/passing-data-to-jwt
-  };
+  xhr.open("POST", url);
 
-  var jwt = sign(user, process.env.JWT_SECRET);
-  var url = new URL(process.env.HUB_URL);
-  url.searchParams.set('auth_token', jwt);
-  console.log('Redirecting to: ', url.toString());
-  return res.redirect(url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  console.log("start");
+
+  xhr.onreadystatechange = function () {
+    console.log("onready");
+    if (xhr.readyState === 4) {
+
+      console.log(xhr.status);
+      var token = xhr.response['access_token'];
+
+      var user = {
+        name: email,
+        email: email,
+        apiKey: `Bearer ${token}`
+      };     
+      var jwt = sign(user, process.env.JWT_SECRET);
+      var url = new URL(process.env.HUB_URL);
+
+      url.searchParams.set('auth_token', jwt);
+      console.log('Redirecting to: ', url.toString());
+
+      return res.redirect(url);
+  }};
+
+  var data = `{
+    email: 'dmytro.d@quali.com',
+    password: 'NKeGVAHwwxTJZ8b'
+  }`;
+  
+  xhr.send(data);
 }
